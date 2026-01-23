ac12a42f32c66a6cdcad056c60cc3e8a6cb3b813
jonpaulauguste <144620333+jonpaulauguste@users.noreply.github.com>
2026-01-23 14:51:40 -0400

    Lab1 Part 6

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ with open('./data.json') as f:
 async def hello_world():
     return 'Hello, World!'
 
-### New Function
-@app.get('/students/{id}')
-async def get_student(id):
-  for student in data: 
-    if student['id'] == id: # Only return the student if the ID matches
-      return student
-### End of new function
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref: # select only the students with a given meal preference
+            filtered_students.append(student)
+        return filtered_students
+    return data
