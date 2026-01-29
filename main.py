2a7e66d8e915f268b1a54e22b03afe54f95dec77
jonpaulauguste <144620333+jonpaulauguste@users.noreply.github.com>
2026-01-28 21:16:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,22 +23,27 @@ async def get_students(pref=None):
         return filtered_students
     return data
 
+
 @app.get('/stats')
 async def get_stats():
-    stats = {}
+    stats = {
+        "Chicken": 0,
+        "Computer Science (Major)": 0,
+        "Computer Science (Special)": 0,
+        "Fish": 0,
+        "Information Technology (Major)": 0,
+        "Information Technology (Special)": 0,
+        "Vegetable": 0
+    }
 
     for student in data:
-        pref = student['pref']
+        pref = student['pref'].strip()
+        programme = student['programme'].strip()
+
         if pref in stats:
             stats[pref] += 1
-        else:
-            stats[pref] = 1
 
-        programme = student['programme']
         if programme in stats:
             stats[programme] += 1
-        else:
-            stats[programme] = 1
-        
-    return stats
 
+    return stats
